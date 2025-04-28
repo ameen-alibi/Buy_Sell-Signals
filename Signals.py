2b67143674cdeb8fdfbbe8c60f80455f3a9d28ed
Ahmed Amine <108481915+ameen-alibi@users.noreply.github.com>
2025-04-28 14:48:03 +0100

    Add RSI,ADX and other indicators

--- Signals.py
+++ Signals.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Signals:
@@ -11,41 +12,111 @@ class Signals:
         self.endDate = end_date
         self.indicators = []
 
-    def SMA(self, window=20):
-        col_name = f"SMA_{window}"
-        if col_name not in self.indicators:
-            self.indicators.append(col_name)
-            self.stockData[col_name] = self.stockData['Close'].rolling(
-                window=window).mean()
-            self.plotIndicator(col_name, label=col_name, color='orange')
-            return "SMA calculated successfully"
-        else:
+    def SMA(self, span=20):
+        col_name = f"SMA_{span}"
+        if col_name in self.indicators:
             return -1
+        self.indicators.append(col_name)
+        self.stockData[col_name] = self.stockData['Close'].rolling(
+            window=span).mean()
+        self.plotIndicator(col_name, label=col_name, color='#f41102')
+        return "SMA calculated successfully"
+
+    def EMA(self, span=20):
+        col_name = f"EMA_{span}"
+        if col_name in self.indicators:
+            return -1
+        self.indicators.append(col_name)
+        self.stockData[col_name] = self.stockData['Close'] \
+            .ewm(span=span, adjust=False) \
+            .mean()
+        self.plotIndicator(col_name, label=col_name, color='orange')
+        return "EMA calculated successfully"
 
     def BBANDS(self, alpha=2):
-        if 'UPPER_BBAND' not in self.indicators:
-            self.indicators.append('UPPER_BBAND')
-            self.SMA()
-            self.STD()
-            self.stockData['UPPER_BBAND'] = self.stockData['SMA_20'] + \
-                self.stockData['STD']*alpha
-            self.plotIndicator(
-                'UPPER_BBAND', label='Higher BBand', color='purple')
-        if 'LOWER_BBAND' not in self.indicators:
-            self.indicators.append('LOWER_BBAND')
-            self.stockData['LOWER_BBAND'] = self.stockData['SMA_20'] - \
-                self.stockData['STD']*alpha
-            self.plotIndicator(
-                'LOWER_BBAND', label='Lower BBand', color='purple')
+        if 'UPPER_BBAND' in self.indicators or 'LOWER_BBAND' in self.indicators:
+            return -1
+        self.indicators.append('UPPER_BBAND')
+        self.SMA()
+        self.STD()
+        self.stockData['UPPER_BBAND'] = self.stockData['SMA_20'] + \
+            self.stockData['STD']*alpha
+        self.plotIndicator(
+            'UPPER_BBAND', label='Higher BBand', color='purple')
+        self.indicators.append('LOWER_BBAND')
+        self.stockData['LOWER_BBAND'] = self.stockData['SMA_20'] - \
+            self.stockData['STD']*alpha
+        self.plotIndicator(
+            'LOWER_BBAND', label='Lower BBand', color='purple')
 
         plt.fill_between(self.stockData.index, self.stockData['UPPER_BBAND'], self.stockData['LOWER_BBAND'],
                          color='#FFD700', alpha=0.15)
 
     def STD(self, window=20):
-        if 'STD' not in self.indicators:
-            self.indicators.append('STD')
-            self.stockData['STD'] = self.stockData['Close'].rolling(
-                window=window).std()
+        if 'STD' in self.indicators:
+            return -1
+        self.indicators.append('STD')
+        self.stockData['STD'] = self.stockData['Close'].rolling(
+            window=window).std()
+
+    def ADX(self, period=14):
+        if 'ADX' in self.indicators:
+            return -1
+
+        col_name = f"ADX_{period}"
+        self.indicators.append(col_name)
+
+        df = self.stockData
+
+        df['h-l'] = df['High'] - df['Low']
+        df['h-pc'] = (df['High'] - df['Close'].shift(1)).abs()
+        df['l-pc'] = (df['Low'] - df['Close'].shift(1)).abs()
+
+        df['TR'] = df[['h-l', 'h-pc', 'l-pc']].max(axis=1)
+        df['up_move'] = df['High'] - df['High'].shift(1)
+        df['down_move'] = df['Low'].shift(1) - df['Low']
+
+        df['+DM'] = np.where((df['up_move'] > df['down_move']) &
+                             (df['up_move'] > 0),
+                             df['up_move'], 0)
+        df['-DM'] = np.where((df['down_move'] > df['up_move']) &
+                             (df['down_move'] > 0),
+                             df['down_move'], 0)
+
+        df['ATR'] = df['TR'].ewm(com=period-1, adjust=False).mean()
+        df['+DM_smooth'] = df['+DM'].ewm(com=period-1, adjust=False).mean()
+        df['-DM_smooth'] = df['-DM'].ewm(com=period-1, adjust=False).mean()
+
+        df['+DI'] = 100 * df['+DM_smooth'] / df['ATR']
+        df['-DI'] = 100 * df['-DM_smooth'] / df['ATR']
+
+        df['DX'] = 100 * (df['+DI'] - df['-DI']).abs() / \
+            (df['+DI'] + df['-DI'])
+        df[col_name] = df['DX'].ewm(com=period-1, adjust=False).mean()
+        df[col_name].iloc[:period] = np.nan
+        self.plotIndicator(col_name, label=col_name, color='white')
+
+    def RSI(self, period=14):
+
+        col_name = f"RSI_{period}"
+        self.indicators.append(col_name)
+
+        df = self.stockData
+
+        delta = df['Close'].diff()
+        gain = np.where(delta > 0, delta, 0)
+        loss = np.where(delta < 0, -delta, 0)
+
+        avg_gain = pd.Series(gain, index=df.index).ewm(
+            alpha=1/period, adjust=False).mean()
+        avg_loss = pd.Series(loss, index=df.index).ewm(
+            alpha=1/period, adjust=False).mean()
+
+        rs = avg_gain/avg_loss
+        rs.iloc[:period] = np.nan
+
+        df[col_name] = 100-(100/(1+rs))
+        self.plotIndicator(col_name, label=col_name, color='silver')
 
     def plotIndicator(self, indicator, label, color):
         if indicator not in self.indicators:
@@ -79,9 +150,14 @@ sns.set_theme(style="darkgrid", rc={
 })
 
 signal = Signals('tesla_price.csv')
-signal.SMA()
+signal.ADX()
+signal.RSI()
 signal.BBANDS()
+signal.SMA()
+signal.EMA()
 ax = signal.plotPrice()
+plt.fill_between(signal.stockData.index, 100, 0,
+                 color='white', alpha=0.15)
 plt.xticks(rotation=45, color='white')
 plt.yticks(rotation=45, color='white')
 plt.title('TSLA inc.', color='white', fontweight='bold')
@@ -91,6 +167,3 @@ plt.legend()
 for legend in ax.legend().get_texts():
     legend.set_color('white')
 plt.show()
-
-# print(signal.get_indicators())
-# print(signal.get_stock_data())
